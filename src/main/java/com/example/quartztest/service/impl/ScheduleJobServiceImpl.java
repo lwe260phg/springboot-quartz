93a3bb68b271d7c82ed3ea06f632950bb6e19b82
lwe260phg <32765428+lwe260phg@users.noreply.github.com>
2025-07-06 12:47:39 +0800

    Update ScheduleJobServiceImpl.java

--- src/main/java/com/example/quartztest/service/impl/ScheduleJobServiceImpl.java
+++ src/main/java/com/example/quartztest/service/impl/ScheduleJobServiceImpl.java
@@ -19,18 +19,7 @@ import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
 
-/**
-*  ScheduleJobServiceImpl 接口实现
- * +----------------------------------------------------------------------
- * | CRMEB [ CRMEB赋能开发者，助力企业发展 ]
- * +----------------------------------------------------------------------
- * | Copyright (c) 2016~2023 https://www.crmeb.com All rights reserved.
- * +----------------------------------------------------------------------
- * | Licensed CRMEB并不是自由软件，未经许可不能去掉CRMEB相关版权
- * +----------------------------------------------------------------------
- * | Author: CRMEB Team <admin@crmeb.com>
- * +----------------------------------------------------------------------
- */
+
 @Service
 public class ScheduleJobServiceImpl extends ServiceImpl<ScheduleJobDao, ScheduleJob> implements ScheduleJobService {
 
