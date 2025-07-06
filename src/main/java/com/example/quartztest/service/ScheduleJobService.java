b1b3fec63f4a73d5133c709b6ea8d02d0589441a
lwe260phg <32765428+lwe260phg@users.noreply.github.com>
2025-07-06 23:01:18 +0800

    new

--- src/main/java/com/example/quartztest/service/ScheduleJobService.java
+++ src/main/java/com/example/quartztest/service/ScheduleJobService.java
@@ -3,18 +3,7 @@ package com.example.quartztest.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.quartztest.entity.ScheduleJob;
 
-/**
-*  ScheduleJobService 接口
- * +----------------------------------------------------------------------
- * | CRMEB [ CRMEB赋能开发者，助力企业发展 ]
- * +----------------------------------------------------------------------
- * | Copyright (c) 2016~2023 https://www.crmeb.com All rights reserved.
- * +----------------------------------------------------------------------
- * | Licensed CRMEB并不是自由软件，未经许可不能去掉CRMEB相关版权
- * +----------------------------------------------------------------------
- * | Author: CRMEB Team <admin@crmeb.com>
- * +----------------------------------------------------------------------
- */
+
 public interface ScheduleJobService extends IService<ScheduleJob> {
 
     /**
